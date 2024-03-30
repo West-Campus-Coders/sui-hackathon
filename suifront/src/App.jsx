1225c975b1679cc26f625af2f3e5e585b4e62385
papagosh <74284165+papagosh@users.noreply.github.com>
2024-03-30 01:12:42 -0700

    added shitty landing page

--- suifront/src/App.jsx
+++ suifront/src/App.jsx
@@ -1,20 +1,26 @@
 
 import './App.css'
 import { ConnectButton } from '@mysten/dapp-kit';
-
+import {Button} from '@mui/material' 
+import casino from './assets/casino.png'
 
 function App() {
  
 
   return (
-    <>
-<div>
-			<nav>
-				<ConnectButton />
-			</nav>
-			<section>Hello, world</section>
-		</div>
-    </>
+<body id='app'>
+ <img src={casino} id='bg'/>
+	<nav>
+		<ConnectButton />
+	</nav>
+	<container className='left-container'>
+		<h1>Choose A Game</h1>
+		<Button variant='contained' color='primary'>BlackJack</Button>
+
+
+	</container>
+</body>
+
   )
 }
 
