6d81f63c14d876a4a2e3af91707511d4ca5df345
GenericJames <james.dundas2@icloud.com>
2024-03-30 16:55:31 -0700

    added game store

---
+++ suifront/src/Store.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import { Component } from "react";
+import Chips from "./Components/Chips";
+
+export default class Store extends Component {
+    render(){
+        return(
+            <div><p>hello</p></div>
+        )
+    }
+
+
+}
